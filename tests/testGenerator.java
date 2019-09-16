0a11bff32335caf867ca9a831a97f9d034062658
Alex Haug <stahh02@moravian.edu>
2019-09-16 17:24:47 -0400

    Added test for sword creation

--- tests/testGenerator.java
+++ tests/testGenerator.java
@@ -10,8 +10,19 @@ public class testGenerator {
     private static Path filePath = Paths.get(currentPath.toString(), "src");
 
     @Test
+    public void testCreateAllSwords() {
+        Weapon bronzeSword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Bronze_Sword.csv");
+        Weapon ironSword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Iron_Sword.csv");
+        Weapon steelSword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Steel_Sword.csv");
+        Weapon silverSword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Silver_Sword.csv");
+        Weapon braveSword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Brave_Sword.csv");
+        Weapon killingEdge = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Killing_Edge.csv");
+        Weapon armorslayer = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Armorslayer.csv");
+        Weapon runesword = new Weapons.Weapon(filePath.toString()+"/Weapons/Swords/Runesword.csv");
+    }
+
+    @Test
     public void testNormalCollosseum() {
-        System.out.println(filePath);
         Creature robin = new Creature("Robin", new CreatureStats(new UnitClass(filePath.toString()+"/Classes/Lord.csv"), 1, 21, 80, 45, 45, 40, 40, 40, 40));
         Creature risen = new Creature("Risen", new CreatureStats(new UnitClass(filePath.toString()+"/Classes/Lord.csv"), 1, 21, 80, 45, 0, 40, 40, 40, 40));
 
