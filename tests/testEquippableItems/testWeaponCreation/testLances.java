bac705c5d1eeb94d477e8caa33b38646107516b0
Alex Haug <stahh02@moravian.edu>
2019-10-02 12:24:44 -0400

    Added Iron Lances and their test

--- tests/testEquippableItems/testWeaponCreation/testLances.java
+++ tests/testEquippableItems/testWeaponCreation/testLances.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import static testEquippableItems.WeaponCreationTestMethod.createAndTestWeapon;
 
 public class testLances {
+
     @Test
     public void testTrainingLance() {
         createAndTestWeapon("Training_Lance", "Training Lance", "Lance",
@@ -16,5 +17,14 @@ public class testLances {
                 false,false, false, new HashSet<String>(),
                 new ArrayList<String>(), "This simple lance is perfect for training purposes. " +
                         "But cannot inflict critical hits." );
+    }
+
+    @Test
+    public void testIronLance() {
+        createAndTestWeapon("Iron_Lance", "Iron Lance", "Lance",
+                6, 80,0,1,1,
+                6,'E', 30,260,false,
+                false,false, false, new HashSet<String>(),
+                new ArrayList<String>(), "A standard lance made of iron--simple but effective." );
     }
 }
