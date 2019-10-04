813e5ac2b777a486130a3e6d22c3a7690caad98c
Alex Haug <stahh02@moravian.edu>
2019-10-04 13:07:11 -0400

    Added Bolganone and its test

--- tests/testEquippableItems/testWeaponCreation/testAnimaMagics.java
+++ tests/testEquippableItems/testWeaponCreation/testAnimaMagics.java
@@ -14,6 +14,16 @@ public class testAnimaMagics {
                 3, 90,0,1,2,
                 3,'E', 10,540,true,
                 false,false, false, new int[9],
-                new ArrayList<String>(), "A basic fire spell.");
+                new ArrayList<String>(), "Basic fire magic.");
     }
+
+    @Test
+    public void testBolganone() {
+        createAndTestWeapon("Bolganone", "Bolganone", "Anima Magic: Fire",
+                8, 80,0,1,2,
+                6,'C', 5,540,true,
+                false,false, false, new int[9],
+                new ArrayList<String>(), "Superior fire magic.");
+    }
+
 }
