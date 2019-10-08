d3a97dc04e6aef0f6d8843d7f3b7f81cb033159f
Alex Haug <ahaug5790@gmail.com>
2019-10-08 15:08:44 -0400

    Added First dark magic

--- tests/testEquippableItems/testWeaponCreation/testDarkMagics.java
+++ tests/testEquippableItems/testWeaponCreation/testDarkMagics.java
@@ -10,9 +10,9 @@ public class testDarkMagics {
     @Test
     public void testFlux() {
         createAndTestWeapon("Flux", "Flux", "Dark Magic",
-                5, 70,0,1,2,
-                5,'E', 45,540,true,
+                5, 75,0,1,2,
+                5,'E', 10,540,true,
                 false,false, false, new int[9],
-                new ArrayList<String>(), "A basic dark magic attack.");
+                new ArrayList<String>(), "Basic dark magic.");
     }
 }
