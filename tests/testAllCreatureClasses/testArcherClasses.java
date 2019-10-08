41498e5e3f879a947acbd5c6f91cc5335673fca0
Alex Haug <stahh02@moravian.edu>
2019-10-08 12:59:14 -0400

    Added Ranger class

--- tests/testAllCreatureClasses/testArcherClasses.java
+++ tests/testAllCreatureClasses/testArcherClasses.java
@@ -21,4 +21,10 @@ public class testArcherClasses {
         Creature sniper = setUpCreature("Robin","Sniper.csv","Ylisee");
         assert Arrays.equals(getClassGrowthRates(sniper), new int[]{10,5,0,20,0,10,0,0,5});
     }
+
+    @Test
+    public void testRanger() {
+        Creature ranger = setUpCreature("Robin","Ranger.csv","Ylisee");
+        assert Arrays.equals(getClassGrowthRates(ranger), new int[]{10,0,0,0,-5,0,0,0,5});
+    }
 }
