cfb1f4d1dfa26e875c0e40bb1e212af737003d7f
Alex Haug <ahaug5790@gmail.com>
2019-10-16 13:42:55 -0400

    Added Steel Gauntlets and its test

--- src/test/java/testEquippableItems/testWeaponCreation/testGauntlets.java
+++ src/test/java/testEquippableItems/testWeaponCreation/testGauntlets.java
@@ -16,7 +16,6 @@ public class testGauntlets {
                 true,false, false, new int[9],
                 new ArrayList<String>(), "Simple gauntlets perfect for training purposes. " +
                         "Wielder strikes twice when initiating combat." );
-
     }
 
     @Test
@@ -27,6 +26,15 @@ public class testGauntlets {
                 true,false, false, new int[9],
                 new ArrayList<String>(), "Standard iron gauntlets-simple but effective. " +
                         "Wielder strikes twice when initiating combat." );
+    }
 
+    @Test
+    public void testSteelGauntlets() {
+        createAndTestWeapon("Steel_Gauntlets", "Steel Gauntlets", "Brawl",
+                3, 80,5,1,1,
+                5,'D', 60,520,false,
+                true,false, false, new int[9],
+                new ArrayList<String>(), "Weighty steel gauntlets that deal heavy blows. " +
+                        "Wielder strikes twice when initiating combat." );
     }
 }
