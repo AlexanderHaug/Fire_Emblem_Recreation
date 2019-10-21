6b4a5fb29a61f48e2db1eb2b6fbc4c947ac838a1
Alex Haug <ahaug5790@gmail.com>
2019-10-21 16:02:19 -0400

    Refactored TileMap to make it more human readable

--- src/main/java/TileMap/TileMap.java
+++ src/main/java/TileMap/TileMap.java
@@ -25,9 +25,9 @@ public class TileMap {
         String filePath = Paths.get(Paths.get(System.getProperty("user.dir")).toString(),
                 "src").toString() + "/main/resources/TileMaps/";
 
-        Creature[][] tileMap = new Creature[20][20];
-        int numberOfColumns = 0;
-        int numberOfRows = 0;
+        Creature[][] genericMap = new Creature[50][50];
+        int finalNumberOfColumns = 0;
+        int finalNumberOfRows = 0;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath+mapFile+".csv"));
@@ -36,26 +36,27 @@ public class TileMap {
 
             while ((row = reader.readLine()) != null) {
                 int arrayColumn = 0;
-                String[] data = row.split(",");
-                for (int x = 0; x < data.length; x++) {
+                String[] lineOfTiles = row.split(",");
 
-                    if (data[x].equals("-")) {
-                        tileMap[arrayRow][arrayColumn] = null;
-                        arrayColumn++;
+                for (String tile : lineOfTiles) {
+
+                    if (tile.equals("-")) {
+                        genericMap[arrayRow][arrayColumn] = null;
+                        arrayColumn += 1;
                     }
 
                     else {
-                        int armyNumber = Integer.parseInt(data[x]);
-                        if (armys.get(armyNumber).size() > getCurrentArmyCount(armyNumber)) {
-                            tileMap[arrayRow][arrayColumn] = getCreature(armyNumber);
-                            arrayColumn++;
+                        int armyNumber = Integer.parseInt(tile);
+                        if (getArmySize(armyNumber) > getCurrentArmyCount(armyNumber)) {
+                            genericMap[arrayRow][arrayColumn] = getCreature(armyNumber);
+                            arrayColumn += 1;
                             incrementArmyTracker(armyNumber);
                         }
                     }
                 }
-                arrayRow++;
-                numberOfColumns = data.length;
-                numberOfRows += 1;
+                arrayRow += 1;
+                finalNumberOfColumns = lineOfTiles.length;
+                finalNumberOfRows += 1;
             }
             reader.close();
         }
@@ -63,54 +64,27 @@ public class TileMap {
         catch (FileNotFoundException e) {System.out.println("The TileMap file was not found.");}
         catch (IOException e) {e.printStackTrace();}
 
-        this.map = createMap(numberOfRows, numberOfColumns, tileMap);
-    }
-
-    private Creature getCreature(int whichArmy) {
-        return armys.get(whichArmy).get(armyDeploymentTracker.get(whichArmy));
-    }
-
-    private int getCurrentArmyCount(int armyNumber) {
-        return armyDeploymentTracker.get(armyNumber);
-    }
-
-    private void incrementArmyTracker(int armyNumber) {
-        armyDeploymentTracker.put(armyNumber, armyDeploymentTracker.get(armyNumber) + 1);
-    }
-
-    private Creature[][] createMap(int rows, int columns, Creature[][] biggerMap) {
-        Creature[][] tileMap = new Creature[rows][columns];
-        for (int x = 0; x < tileMap.length; x++) {
-            for (int y = 0; y < tileMap[0].length; y++) {
-                tileMap[x][y] = biggerMap[x][y];
-            }
-        }
-        return tileMap;
+        this.map = createFinalMap(finalNumberOfRows, finalNumberOfColumns, genericMap);
     }
 
     public Creature[][] getMap() {return this.map;}
 
-    public void placeCreatureOnMap(int row, int column, Creature creature) {
-        if (this.map[row][column] == null) {
-            this.map[row][column] = creature;
-        }
-        else {
-            System.out.println("This tile is already occupied.");
-        }
-    }
-
     public ArrayList<ArrayList<Creature>> getArmys() {return this.armys;}
+
+    public void placeCreatureOnMap(int row, int column, Creature creature) {
+
+        if (this.map[row][column] == null) {this.map[row][column] = creature;}
+
+        else {System.out.println("This tile is already occupied.");}
+    }
 
     public void displayBattleMap() {
         System.out.println("Current Battle Map");
         for (int x = 0; x < map.length; x++) {
-            if (x != 0) {
-                System.out.println();
-            }
+            if (x != 0) {System.out.println();}
+
             for (int y = 0; y < map.length; y++) {
-                if (this.map[x][y] == null) {
-                    System.out.print(" - ");
-                }
+                if (this.map[x][y] == null) {System.out.print(" - ");}
 
                 else {
                     System.out.print(this.map[x][y].getCreatureName().charAt(0) + ":" +
@@ -118,5 +92,25 @@ public class TileMap {
                 }
             }
         }
+    }
+
+    private Creature getCreature(int whichArmy) {
+        return armys.get(whichArmy).get(armyDeploymentTracker.get(whichArmy));
+    }
+
+    private int getCurrentArmyCount(int armyNumber) {return armyDeploymentTracker.get(armyNumber);}
+
+    private void incrementArmyTracker(int armyNumber) {
+        armyDeploymentTracker.put(armyNumber, armyDeploymentTracker.get(armyNumber) + 1);
+    }
+
+    private int getArmySize(int armyNumber) {return armys.get(armyNumber).size();}
+
+    private Creature[][] createFinalMap(int rows, int columns, Creature[][] biggerMap) {
+        Creature[][] tileMap = new Creature[rows][columns];
+        for (int x = 0; x < tileMap.length; x++) {
+            System.arraycopy(biggerMap[x], 0, tileMap[x], 0, tileMap[0].length);
+        }
+        return tileMap;
     }
 }
