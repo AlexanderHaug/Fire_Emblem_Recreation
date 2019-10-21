f9348fae80e5f3679431528f3d09a5a60b73edf3
Alex Haug <ahaug5790@gmail.com>
2019-10-21 15:42:25 -0400

    Deploying units now works for non square maps

--- src/main/java/TileMap/TileMap.java
+++ src/main/java/TileMap/TileMap.java
@@ -13,19 +13,12 @@ import java.util.HashMap;
 public class TileMap {
     private ArrayList<ArrayList<Creature>> armys;
     private Creature[][] map;
-    private HashMap<Integer, Integer> armyPlacementTracker = new HashMap<>();
+    private HashMap<Integer, Integer> armyDeploymentTracker = new HashMap<>();
 
     public TileMap(ArrayList<ArrayList<Creature>> armys, String mapFile) {
         this.armys = armys;
-        for (int x = 0; x < this.armys.size(); x++) {
-            armyPlacementTracker.put(x, 0);
-        }
+        for (int x = 0; x < this.armys.size(); x++) {armyDeploymentTracker.put(x, 0);}
         setMap(mapFile);
-    }
-
-    public TileMap(ArrayList<ArrayList<Creature>> armys, int[] mapSize) {
-        this.armys = armys;
-        this.map = new Creature[mapSize[0]][mapSize[1]];
     }
 
     private void setMap(String mapFile) {
@@ -33,8 +26,8 @@ public class TileMap {
                 "src").toString() + "/main/resources/TileMaps/";
 
         Creature[][] tileMap = new Creature[20][20];
-        int rowBound = 0;
-        int columnBound = 0;
+        int numberOfColumns = 0;
+        int numberOfRows = 0;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath+mapFile+".csv"));
@@ -45,23 +38,24 @@ public class TileMap {
                 int arrayColumn = 0;
                 String[] data = row.split(",");
                 for (int x = 0; x < data.length; x++) {
+
                     if (data[x].equals("-")) {
                         tileMap[arrayRow][arrayColumn] = null;
                         arrayColumn++;
                     }
+
                     else {
-                        if (armys.get(Integer.parseInt(data[x])).size() >
-                                armyPlacementTracker.get(Integer.parseInt(data[x]))) {
-                            tileMap[arrayRow][arrayColumn] = getCreature(Integer.parseInt(data[x]),
-                                    Integer.parseInt(data[x]));
+                        int armyNumber = Integer.parseInt(data[x]);
+                        if (armys.get(armyNumber).size() > getCurrentArmyCount(armyNumber)) {
+                            tileMap[arrayRow][arrayColumn] = getCreature(armyNumber);
                             arrayColumn++;
-                            armyPlacementTracker.put(x, armyPlacementTracker.put(Integer.parseInt(data[x]), x+1));
+                            incrementArmyTracker(armyNumber);
                         }
                     }
                 }
                 arrayRow++;
-                rowBound = data.length;
-                columnBound += 1;
+                numberOfColumns = data.length;
+                numberOfRows += 1;
             }
             reader.close();
         }
@@ -69,17 +63,25 @@ public class TileMap {
         catch (FileNotFoundException e) {System.out.println("The TileMap file was not found.");}
         catch (IOException e) {e.printStackTrace();}
 
-        this.map = createMap(rowBound,columnBound,tileMap);
+        this.map = createMap(numberOfRows, numberOfColumns, tileMap);
     }
 
-    private Creature getCreature(int whichArmy, int currentArmyMember) {
-        return armys.get(whichArmy).get(armyPlacementTracker.get(currentArmyMember));
+    private Creature getCreature(int whichArmy) {
+        return armys.get(whichArmy).get(armyDeploymentTracker.get(whichArmy));
+    }
+
+    private int getCurrentArmyCount(int armyNumber) {
+        return armyDeploymentTracker.get(armyNumber);
+    }
+
+    private void incrementArmyTracker(int armyNumber) {
+        armyDeploymentTracker.put(armyNumber, armyDeploymentTracker.get(armyNumber) + 1);
     }
 
     private Creature[][] createMap(int rows, int columns, Creature[][] biggerMap) {
         Creature[][] tileMap = new Creature[rows][columns];
         for (int x = 0; x < tileMap.length; x++) {
-            for (int y = 0; y < tileMap.length; y++) {
+            for (int y = 0; y < tileMap[0].length; y++) {
                 tileMap[x][y] = biggerMap[x][y];
             }
         }
--- src/test/java/testTileMap.java
+++ src/test/java/testTileMap.java
@@ -1,5 +1,5 @@
-import TileMap.TileMap;
 import Creatures.Creature;
+import TileMap.TileMap;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,39 +10,7 @@ import static testSetUpMethods.createCreatureSetUp.createArmy;
 public class testTileMap {
 
     @Test
-    public void testPlaceCreatureOnOccupied() {
-
-        ArrayList<ArrayList<Creature>> armys = new ArrayList<>();
-        armys.add(createArmy(1, new String[]{"Cleric"}, "Red"));
-        armys.add(createArmy(1, new String[]{"Shaman"}, "Blue"));
-        TileMap battleMap = new TileMap(armys, new int[]{3,3});
-        battleMap.placeCreatureOnMap(0,1, armys.get(0).get(0));
-        battleMap.placeCreatureOnMap(0,1, armys.get(1).get(0));
-
-        assert battleMap.getMap()[0][1].equals(armys.get(0).get(0));
-    }
-
-    @Test
-    public void testTileMapWith2Teams() {
-
-        ArrayList<ArrayList<Creature>> armys = new ArrayList<>();
-        armys.add(createArmy(1, new String[]{"Cleric"}, "Red"));
-        armys.add(createArmy(1, new String[]{"Shaman"}, "Blue"));
-        TileMap battleMap = new TileMap(armys, new int[]{3,3});
-
-        battleMap.placeCreatureOnMap(0,1, armys.get(0).get(0));
-        battleMap.placeCreatureOnMap(2,1, armys.get(1).get(0));
-
-        Creature[][] testMap = new Creature[3][3];
-        testMap[0][1] = armys.get(0).get(0);
-        testMap[2][1] = armys.get(1).get(0);
-
-        assert battleMap.getArmys().equals(armys);
-        assert Arrays.deepEquals(battleMap.getMap(), testMap);
-    }
-
-    @Test
-    public void testTileMapWithCSV() {
+    public void testTileMap3x3() {
 
         ArrayList<ArrayList<Creature>> armys = new ArrayList<>();
         armys.add(createArmy(2, new String[]{"Cleric"}, "Red"));
@@ -55,5 +23,39 @@ public class testTileMap {
 
         assert battleMap.getArmys().equals(armys);
         assert Arrays.deepEquals(battleMap.getMap(), testMap);
+    }
+
+    @Test
+    public void testTileMap5x3() {
+
+        ArrayList<ArrayList<Creature>> armys = new ArrayList<>();
+        armys.add(createArmy(3, new String[]{"Cleric"}, "Red"));
+        armys.add(createArmy(3, new String[]{"Shaman"}, "Blue"));
+        TileMap battleMap = new TileMap(armys, "5x3");
+
+        Creature[][] testMap = new Creature[5][3];
+
+        testMap[0][0] = armys.get(0).get(0);
+        testMap[0][2] = armys.get(0).get(1);
+        testMap[1][1] = armys.get(0).get(2);
+
+        testMap[3][1] = armys.get(1).get(0);
+        testMap[4][0] = armys.get(1).get(1);
+        testMap[4][2] = armys.get(1).get(2);
+
+        assert battleMap.getArmys().equals(armys);
+        assert Arrays.deepEquals(battleMap.getMap(), testMap);
+    }
+
+    @Test
+    public void testPlaceCreatureOnOccupied() {
+
+        ArrayList<ArrayList<Creature>> armys = new ArrayList<>();
+        armys.add(createArmy(1, new String[]{"Cleric"}, "Red"));
+        armys.add(createArmy(1, new String[]{"Shaman"}, "Blue"));
+        TileMap battleMap = new TileMap(armys, "3x3");
+        battleMap.placeCreatureOnMap(0,1, armys.get(1).get(0));
+
+        assert battleMap.getMap()[0][1].equals(armys.get(0).get(0));
     }
 }
