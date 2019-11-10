4896d3d8580656424c461ec90784d1f37d9e4fcf
Alex Haug <ahaug5790@gmail.com>
2019-11-10 13:22:20 -0500

    Dark Magics now extends an actual abstract class

--- src/main/java/Items/Equippable/MainHand/Dark_Magics/Flux.java
+++ src/main/java/Items/Equippable/MainHand/Dark_Magics/Flux.java
@@ -2,25 +2,23 @@ package Items.Equippable.MainHand.Dark_Magics;
 
 import Items.Equippable.MainHand.Weapon;
 
+import java.util.HashMap;
+
 public class Flux extends Weapon {
 
     public Flux() {
-        setName("Flux");
-        setItemType("Dark Magic");
-
-        setMight(5);
-        setAccuracy(75);
-        setCritical(0);
-        setItemsRange(new int[]{1,2});
-        setItemWeight(5);
-        setItemRank('E');
-
-        setItemUses(10);
-        setItemCost(540);
-        setItemIs("Magic");
-
-        setStatModifiers(new int[]{0,0,0,0,0,0,0,0,0});
-        setEffectiveAgainst(new String[]{});
-        setItemDescription("Basic dark magic.");
+        this.name = "Flux";
+        this.type = "Dark Magic";
+        this.might = 5;
+        this.accuracy = 75;
+        this.critical = 0;
+        this.range = new int[]{1,2};
+        this.weight = 5;
+        this.rank = 'E';
+        this.uses = 10;
+        this.cost = 540;
+        this.additionalAbilities = new HashMap<>();
+        this.additionalAbilities.put("Magic", 1);
+        this.description = "Basic dark magic.";
     }
 }
--- src/main/java/Items/Equippable/MainHand/Dark_Magics/Mire.java
+++ src/main/java/Items/Equippable/MainHand/Dark_Magics/Mire.java
@@ -2,25 +2,25 @@ package Items.Equippable.MainHand.Dark_Magics;
 
 import Items.Equippable.MainHand.Weapon;
 
+import java.util.HashMap;
+
 public class Mire extends Weapon {
 
     public Mire() {
-        setName("Mire");
-        setItemType("Dark Magic");
-
-        setMight(3);
-        setAccuracy(70);
-        setCritical(0);
-        setItemsRange(new int[]{1,3});
-        setItemWeight(5);
-        setItemRank('D');
-
-        setItemUses(8);
-        setItemCost(540);
-        setItemIs("MagicDebuff:0:0:0:0:0:0:5:0:0");
-
-        setStatModifiers(new int[]{0,0,0,0,0,0,0,0,0});
-        setEffectiveAgainst(new String[]{});
-        setItemDescription("Intermediate dark magic. Inflicts Def -5 to foe for 1 turn.");
+        this.name = "Mire";
+        this.type = "Dark Magic";
+        this.might = 3;
+        this.accuracy = 70;
+        this.critical = 0;
+        this.range = new int[]{1,3};
+        this.weight = 5;
+        this.rank = 'D';
+        this.uses = 8;
+        this.cost = 540;
+        this.additionalAbilities = new HashMap<>();
+        this.additionalAbilities.put("Magic", 1);
+        this.additionalAbilities.put("Debuff", 1);
+        this.additionalAbilities.put("Temp Debuff: Def", -5);
+        this.description = "Intermediate dark magic. Inflicts Def -5 to foe for 1 turn.";
     }
 }
